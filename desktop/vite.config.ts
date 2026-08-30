e03e007c821d7e1a23adb00d05ff1d119079a282
reyneralegria13 <reyner.alegria@ufam.edu.br>
2026-08-30 00:07:38 -0400

    🔧 build(desktop): configure the audio worklet plugin

--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { noiseSuppressionAudioWorkletVitePlugin } from '@workadventure/noise-suppression/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), noiseSuppressionAudioWorkletVitePlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
